730c19d05817febc0dc9eceef687d310ca1d61f9
Fish <fisherbichboy@gmail.com>
2026-07-17 10:14:01 -0400

    ready for merge

--- assets/sprites/HackNSlash Assets/fishermovedemo.gd
+++ assets/sprites/HackNSlash Assets/fishermovedemo.gd
@@ -1,8 +1,5 @@
 extends CharacterBody2D
 
-var health = 100
-@export var cooldown:Dictionary = {"current":0, "reset":120}
-var inrange:Array[CharacterBody2D]
 
 const SPEED = 300.0
 
@@ -22,29 +19,5 @@ func _physics_process(delta: float) -> void:
 		velocity.y = directiony * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-		
+
 	move_and_slide()
-	
-	if cooldown["current"] > 0:
-		cooldown["current"] -= 1
-	else:
-		if Input.is_action_just_pressed("grapple"):
-			print_debug(inrange)
-			cooldown["current"] = cooldown["reset"]
-			for I in inrange:
-				I.enhealth -= 5
-				I.checkifdead()
-				I.cooldown["current"] = I.cooldown["reset"]
-				print_debug(I.enhealth)
-	
-func checkifdead():
-	if health <= 0:
-		get_tree().reload_current_scene()
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	inrange.append(body)
-
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	inrange.erase(body)
