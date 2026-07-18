ed0bef35720d62e09e641e4186426e55c6bd3820
OmniKami <artemisjohnstonworthen@gmail.com>
2026-07-18 09:59:31 -0400

    Fixes. Yum! I am gay btw

--- assets/sprites/HackNSlash Assets/fishermovedemo.gd
+++ assets/sprites/HackNSlash Assets/fishermovedemo.gd
@@ -5,12 +5,15 @@ extends CharacterBody2D
 @export var speed: float = 300.0
 @export var cooldown: Dictionary[String,int] = {"current":0,"reset":60}
 @export var sprite: AnimatedSprite2D
+var direction
+var facing = 1
 var in_range: Array[CharacterBody2D]
 
 func _physics_process(delta: float) -> void:
 
 	# Movement
-	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	direction = Input.get_vector("walk_left","walk_right","walk_up","walk_down")
+	facing = Input.get_axis("walk_left", "walk_right")
 	velocity = speed * direction
 	move_and_slide()
 	
@@ -27,6 +30,7 @@ func _physics_process(delta: float) -> void:
 				i.checkifdead()
 		else:
 			sprite.play("static")
+		_draw()
 
 # Add / remove enemy from range
 func _on_attack_area_entered(body: Node2D) -> void:
@@ -39,3 +43,10 @@ func _on_attack_area_exited(body: Node2D) -> void:
 func checkifdead():
 	if health <= 0:
 		get_tree().reload_current_scene()
+		
+func _draw() -> void:
+	if facing == -1:
+		$AnimatedSprite2D.scale.x = -1
+	if facing == 1:
+		$AnimatedSprite2D.scale.x = 1
+		
